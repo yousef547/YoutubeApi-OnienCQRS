4f0931eb38579caaa384b9c89da3a9429b5fc53c
Yhafez <yhafez@stingrayconsultation.com>
2024-09-07 18:07:34 +0300

    complete entity

---
+++ Core/YoutubeApi.Domain/Common/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Domain.Common.Entities
+{
+    public class Brand : EntityBase, IEntityBase
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
